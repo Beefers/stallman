4610b3cdb424aed1e9b2c8e85401bf123e084e8c
rem-my <ethandavidbarrow@outlook.com>
2021-08-18 00:41:21 +0100

    Pushing changes from when I had no internet

--- handlers/permissions.js
+++ handlers/permissions.js
@@ -3,13 +3,13 @@ async function init(client) {
     if (!client.application?.owner) client.application?.fetch();
     
     client.config.routes.servers.forEach(id => {
-        client.guilds.cache.get(id).commands.fetch()
-            .then(commands => commands.forEach(command => {
-                fullPermissions.push({
-                    id: command.id,
-                    permissions: client.localCommands.filter(localCommand => localCommand.name === command.name).permssions,
-                })
-            }))
+        client.localCommands.forEach(localCommand => {
+            fullPermissions.push({
+                id: client.guilds.cache.get(id).commands.fetch().then(commands => commands.filter(command => command.name === localCommand.name).id),
+                permissions: localCommand.permssions || '',
+            })
+            console.log(fullPermissions)
+        })
         client.guilds.cache.get(id)?.commands.permissions.set({ fullPermissions });
     })
 }
